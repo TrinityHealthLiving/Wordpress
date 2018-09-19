7255d89c5f72340faf46650b0faab0887956865a
TrinityHealthLiving <43397617+TrinityHealthLiving@users.noreply.github.com>
2018-09-19 15:21:30 +1000

    Update index.php

--- wp-content/themes/anissa/index.php
+++ wp-content/themes/anissa/index.php
@@ -36,6 +36,35 @@ get_header(); ?>
   <!-- #main --> 
 	<script type="text/javascript" src="http://trinityhealthandliving.com.au/wp-content/themes/anissa/js/owl.carousel.js?ver=20120206"></script>
 </div>
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @package anissa
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div class="entry-cat">
+			&mdash; <?php the_category( ', ' ) ?> &mdash;
+		</div><!-- .entry-cat -->
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+	<div class="entry-datetop">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a>
+		</div><!-- .entry-datetop -->
+		<?php if ( has_post_thumbnail() ) : ?>
+			<div class="featured-image">
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'anissa-home' ); ?></a>         
+			</div>
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-excerpt -->
+
+</article><!-- #post-## -->
 <!-- #primary -->
 
 <?php get_sidebar(); ?>
